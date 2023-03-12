0ddb740355f634e07905e31a6d89aeef85ae826b
Ashwin nair <95960077+geek212@users.noreply.github.com>
2023-03-12 19:52:11 +0530

    Add files via upload

---
+++ import_modules.py
@@ -0,0 +1,57 @@
+import os
+print (os.listdir())
+
+import print
+import Set_datatype
+# u can import ur code files with import
+
+''' The listdir() function is a function provided by the os (operating system)module, 
+and by using this function, we can print a list of names of all the files present in the specified path.'''
+
+
+
+
+# What are Python Modules?
+''' In Python, Modules are simply files with the “.py” extension containing Python code 
+that can be imported inside another Python Program. '''
+
+
+# How to create Python Modules?
+''' To create a module, we have to save the code that we wish in a file with
+ the file extension “.py”. Then, the name of the Python file becomes the name of the module.'''
+
+
+
+# Advantages of Modules.
+# Some of the advantages while working with modules in Python is as follows:
+
+# Reusability
+# Working with modules makes the code reusable.
+
+# Simplicity
+# The module focuses on a small proportion of the problem, rather than focusing on the entire problem.
+
+# Scoping
+# A separate namespace is defined by a module that helps to avoid collisions between identifiers.
+ 
+
+
+''' Python Built-in Modules'''
+# As we know that the Python interactive shell has a number of built-in functions. 
+# As a shell start, these functions are loaded automatically and are always available, such as,
+
+# print() and input() for I/O,
+# Number conversion functions such as int(), float(), complex(),
+# Data type conversions such as list(), tuple(), set(), etc.
+
+
+# In addition to these many built-in functions, there are also a large number of pre-defined 
+# functions available as a part of libraries bundled with Python distributions. 
+# These functions are defined in modules which are known as built-in modules
+
+
+help ("modules")
+# These built-in modules are written in C language and integrated with the Python shell.
+ 
+
+
